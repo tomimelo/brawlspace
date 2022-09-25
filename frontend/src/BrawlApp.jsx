b860d3c556bfbd54e9c252ee1ff65d46d6bf9f84
Facundo <floresfacundo68@gmail.com>
2022-09-25 19:28:45 -0300

    add form to front-end
    Co-authored-by: Tomas Melone <tomimelo@users.noreply.github.com>

--- frontend/src/BrawlApp.jsx
+++ frontend/src/BrawlApp.jsx
@@ -1,13 +1,31 @@
-import { Button, Center, Input, Text } from '@chakra-ui/react'
+import { Button, Container, Input, Text } from '@chakra-ui/react'
+import { useForm } from './hooks'
 
 export const BrawlApp = () => {
+
+  const { searchText, onInputChange, onResetForm } = useForm({searchText: ''});
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    console.log(searchText);
+    onResetForm();
+  }
+
   return (
     <>
-      <Center bg='tomato' h='100px' color='white' >
+      <Container maxW='container.lg' >
         <Text fontSize='5xl'>BrawlApp</Text>
-        <Input placeholder='large size' size='lg' />
-        <Button>Search</Button>
-      </Center>
+        <form onSubmit={ onSearchSubmit } aria-label="form">
+          <Input 
+            name="searchText" 
+            value={ searchText } 
+            onChange={ onInputChange } 
+            placeholder='Busca tu perfil' 
+            size='lg' 
+          />
+          <Button type="submit">Search</Button>
+        </form>
+      </Container >
     </>
   )
 }
