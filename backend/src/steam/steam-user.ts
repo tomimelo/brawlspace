b2e267f2835d32262e804126480984dd7aa3e887
Tomas Melone <tomasmelone@hotmail.com>
2022-09-30 14:33:22 +0200

    split steam user interface

--- backend/src/steam/steam-user.ts
+++ backend/src/steam/steam-user.ts
@@ -1,10 +1,15 @@
-export interface SteamUser {
-  url: string;
-  name?: string;
-  alias: string;
-  image: string;
+export interface AvatarInfo {
+  url: string,
+  image: string
+}
+
+export interface UserInfo {
+  alias: string,
+  name?: string,
   location?: {
-    description: string;
-    image: string;
+    description: string,
+    image: string
   }
 }
+
+export type SteamUser = AvatarInfo & UserInfo
