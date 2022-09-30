3b5dcbd7dc8d9fd0ac30919674544a59c12995ec
Tomas Melone <tomasmelone@hotmail.com>
2022-09-30 03:01:48 +0200

    add apis interface

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -10,6 +10,12 @@ export interface AppConfig {
     password: string;
     options: any;
   };
+  brawlhalla: {
+    apiKey: string
+  };
+  steam: {
+    apiKey: string
+  };
   logger: {
     level: string;
   };
