748fc0a19f31e46b1953211570e1624cddc03490
Tomas Melone <tomasmelone@hotmail.com>
2022-09-30 18:17:34 +0200

    add not found error

---
+++ backend/src/error/errors.ts
@@ -0,0 +1,10 @@
+import { MadError } from 'mad-error'
+
+export class NotFoundError extends MadError {
+  public constructor (message: string) {
+    super(message)
+    this.name = 'NotFoundError'
+    this.code('NOT_FOUND')
+    this.status(404)
+  }
+}
