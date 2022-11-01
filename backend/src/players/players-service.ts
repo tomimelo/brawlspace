3f46b7253bfd205d70b553daf1f4ec1d61f1f385
Tomas Melone <tomasmelone@hotmail.com>
2022-11-01 19:23:06 +0100

    update steam explorer lib

--- backend/src/players/players-service.ts
+++ backend/src/players/players-service.ts
@@ -1,20 +1,10 @@
-import { SteamExplorer, SteamUser } from 'steam-explorer'
+import { SteamExplorer, SteamUser, FindOptions, FindResults } from 'steam-explorer'
 import { BrawlhallaClient } from '../brawlhalla/brawlhalla-client'
-
-export interface SearchOptions {
-  page: number
-}
-
-export interface SearchResults<T> {
-  total: number
-  results: ReadonlyArray<T>
-  page: number
-}
 
 export class PlayersService {
   public constructor(private readonly steamExplorer: SteamExplorer, private readonly brawlhallaClient: BrawlhallaClient) {}
 
-  public async searchPlayers(q: string, options: SearchOptions): Promise<SearchResults<SteamUser>> {
+  public async searchPlayers(q: string, options: FindOptions): Promise<FindResults<SteamUser>> {
     return this.steamExplorer.findUsers(q, options)
   }
 
