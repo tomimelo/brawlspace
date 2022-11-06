8146eea1187fefe7b1ecffc871b38eba0c4c6342
Tomas Melone <tomasmelone@hotmail.com>
2022-11-06 21:30:00 +0100

    fix steam id conversion

--- backend/src/utils/steam-utils.ts
+++ backend/src/utils/steam-utils.ts
@@ -23,21 +23,20 @@ export class SteamUtils {
     let steamId: string = value
     if (this.isSteamID3(value)) {
       steamId = this.fromSteamID3(value)
-    }
-
-    if (!this.isSteamID(value)) {
+    } else if (!this.isSteamID(value)) {
       throw new Error('The value provided is not a valid Steam ID')
     }
 
     const v = BigInt('76561197960265728')
     const [, y, z] = steamId.split(':')
 
-    return (BigInt(Number(z) * 2) + v + y).toString()
+    return (BigInt(Number(z) * 2) + v + BigInt(y)).toString()
   }
 
   private static fromSteamID3(value: string): string {
     const [, , w] = value.split(':')
-    const reduced = Number(w.substring(0, w.length - 1))
-    return 'STEAM_0:' + (reduced % 2) + ':' + Math.floor(reduced / 2)
+    const W = Number(w)
+    const accountType = W % 2
+    return 'STEAM_0:' + accountType.toString() + ':' + Math.floor(W / 2).toString()
   }
 }
